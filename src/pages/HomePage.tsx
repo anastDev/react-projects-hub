5368dbe0b271d2c2020e249cfbc4240d917e3e40
anastasiam <anast.moud@gmail.com>
2025-11-11 22:29:45 +0200

    style: added hover styling and shadow

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -2,9 +2,7 @@ import {useEffect} from "react";
 import {Link} from "react-router";
 
 const HomePage = () => {
-    const links = [
-        {path: "/", label: "Homepage"},
-        {path: "/calculator-app", label: "Calculator App"},
+    const links = [{path: "/calculator-app", label: "Calculator App"},
         {path: "/task-manager-app", label: "Task Manager App"},
         {path: "/weather-app", label: "Weather App"},
         {path: "/movie-search-app", label: "Movie Search App"},
@@ -18,7 +16,7 @@ const HomePage = () => {
 
     return (
         <>
-            <div className="container w-1/2 mx-auto border-2 border-black mt-24 p-8 rounded-lg">
+            <div className="container cursor-default bg-gray-100 w-1/2 mx-auto mt-28 p-8 rounded-lg shadow-md">
                 <h1 className="text-center text-2xl mb-2">React Project Hub</h1>
                 <p className="text-md">A collection of React projects, built to practice and showcase my knowledge of components, hooks, routing and real-world app structure.</p>
                 <div className="text-center flex flex-col gap-4 mt-4">
@@ -26,7 +24,7 @@ const HomePage = () => {
                         <Link
                             key={link.path}
                             to={link.path}
-                            className=" bg-gray-800 text-white px-6 py-2 rounded-md"
+                            className=" bg-gray-900 hover:bg-gray-700 text-white px-6 py-2 rounded-md"
                         >{link.label}</Link>
                     ))}
                 </div>
