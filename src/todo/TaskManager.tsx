d7c0c5660c9135b84dcf3bf6f185bfafde70a25d
anastasiam <anast.moud@gmail.com>
2025-11-12 13:27:57 +0200

    style: add container border, height and overflow

--- src/todo/TaskManager.tsx
+++ src/todo/TaskManager.tsx
@@ -1,6 +1,7 @@
 import TaskForm from "./components/TaskForm.tsx";
 import {useEffect, useRef, useState} from "react";
 import type {TaskProps} from "../types.tsx";
+import TasksList from "./components/TasksList.tsx";
 
 const TaskManager = () => {
     const [tasks, setTasks] = useState<TaskProps[]>([]);
@@ -13,22 +14,49 @@ const TaskManager = () => {
                 id: Date.now(),
                 text,
                 completed: false},
-        ])
+        ]);
+    }
+
+    const editTask = (id: number, newText: string) => {
+        setTasks((prev) =>
+            prev.map(
+                task => task.id === id ? {...task, text: newText} : task));
+    }
+
+    const toggleTask = (id: number) => {
+        setTasks(prev =>
+            prev.map(
+                task => task.id === id ? {...task, completed: !task.completed} : task));
+    }
+
+    const deleteTask = (id: number) => {
+        setTasks( (prev) =>
+            prev.filter(task => task.id !== id));
     }
 
     useEffect(() => {
         inputRef.current?.focus();
-    }, []);
+    }, [tasks]);
 
     return (
         <>
-            <div className="container mt-28 max-w-sm mx-auto">
-                <h1 className="text-black text-2xl">Your Personal Task Manager</h1>
-                {/*    Form  */}
-                <TaskForm
-                    addTask={addTask}
-                    inputRef={inputRef}
+            <div className="container border-1 h-[32rem] max-w-md px-4 py-6 mt-28 mx-auto overflow-hidden rounded-xl">
+                <div>
+                    <h1 className="text-black text-2xl">Your Personal Task Manager</h1>
+                    {/*    Input Form  */}
+                    <TaskForm
+                        addTask={addTask}
+                        inputRef={inputRef}
+                    />
+                </div>
+                <TasksList
+                tasks={tasks}
+                editTask={editTask}
+                toggleTask={toggleTask}
+                deleteTask={deleteTask}
                 />
+                {/*  Container of Tasks  */}
+                {/*  Status of Tasks  */}
 
             </div>
         </>
