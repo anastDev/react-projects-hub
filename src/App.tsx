e8890f951227bd85e1547f5f4a31ad86534b2cc6
anastasiam <anast.moud@gmail.com>
2025-11-21 21:53:18 +0200

    feat: add WeatherApp route to app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import {BrowserRouter, Route, Routes} from "react-router";
 import HomePage from "./pages/HomePage.tsx";
 import TaskManager from "./todo/TaskManager.tsx";
+import WeatherApp from "@/weatherapp/WeatherApp.tsx";
 
 function App() {
 
@@ -12,6 +13,7 @@ function App() {
             <Route index element={<HomePage />} />
             <Route path="projects?">
                 <Route path="task-manager-app" element={<TaskManager/>}/>
+                <Route path="weather-app" element={<WeatherApp/>}/>
             </Route>
         </Routes>
     </BrowserRouter>
---
+++ src/weatherapp/WeatherApp.tsx
@@ -0,0 +1,14 @@
+import Header from "@/weatherapp/components/layout/Header.tsx";
+
+const WeatherApp = () => {
+    return (
+        <>
+        {/*  Header with search field  */}
+           <Header/>
+        {/*  Main Content  */}
+        {/*  Footer: data provided by*/}
+        </>
+    )
+}
+
+export default WeatherApp;
