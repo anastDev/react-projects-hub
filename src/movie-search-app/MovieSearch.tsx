69c0e9e2b6bd723ee4e3d911a671aa57e4e153ed
anastasiam <anast.moud@gmail.com>
2025-11-26 00:39:32 +0200

    feat: add MovieSearch project

---
+++ src/movie-search-app/MovieSearch.tsx
@@ -0,0 +1,17 @@
+import TextFieldSearchMovie from "@/movie-search-app/components/TextFieldSearchMovie.tsx";
+
+const MovieSearch = () => {
+    return (
+        <>
+            <body className="bg-indigo-100">
+
+            {/* Search Field */}
+            <TextFieldSearchMovie/>
+
+            {/*  Container of the result-movie  */}
+            </body>
+        </>
+    )
+}
+
+export default MovieSearch;
