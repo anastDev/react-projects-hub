029e60f0b48ed3f2cb68a55e6f1a3a7b09f1d6b7
anastasiam <anast.moud@gmail.com>
2025-11-26 23:46:04 +0200

    chore: set custom document title for the page

--- src/movie-search-app/MovieSearch.tsx
+++ src/movie-search-app/MovieSearch.tsx
@@ -1,14 +1,20 @@
 import TextFieldSearchMovie from "@/movie-search-app/components/TextFieldSearchMovie.tsx";
+import MovieContainer from "@/movie-search-app/components/MovieContainer.tsx";
+import {useEffect} from "react";
 
 const MovieSearch = () => {
+
+    useEffect(() => {
+        document.title = "Movie Search App";
+    }, [])
+
     return (
         <>
             <body className="bg-indigo-100">
-
             {/* Search Field */}
             <TextFieldSearchMovie/>
-
             {/*  Container of the result-movie  */}
+            <MovieContainer/>
             </body>
         </>
     )
