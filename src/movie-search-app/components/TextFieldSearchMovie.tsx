668a9d013431af889907ba8e335a3d1fb74d4f84
anastasiam <anast.moud@gmail.com>
2025-11-26 23:47:31 +0200

    feat(header): add Search button and update container width and flex layout

--- src/movie-search-app/components/TextFieldSearchMovie.tsx
+++ src/movie-search-app/components/TextFieldSearchMovie.tsx
@@ -1,17 +1,27 @@
 import {SearchIcon} from "lucide-react";
 import {InputGroup, InputGroupAddon, InputGroupInput} from "@/components/ui/input-group.tsx";
+import {Button} from "@/components/ui/button"
 
 const TextFieldSearchMovie = () => {
+
+    // TODO: Add focus to useEffect for the search field
     return (
         <>
             <header>
-                <div className="container w-1/2 mx-auto mt-4">
-                    <InputGroup className="bg-indigo-200">
-                        <InputGroupInput placeholder="Search movie..." />
-                        <InputGroupAddon>
-                            <SearchIcon className="color-black" />
-                        </InputGroupAddon>
-                    </InputGroup>
+                <div className="md:container w-full mx-auto mt-4 flex flex-row space-x-2">
+                   <div className="w-full flex-1">
+                       <InputGroup className="bg-indigo-200">
+                           <InputGroupInput placeholder="Search movie..." />
+                           <InputGroupAddon>
+                               <SearchIcon className="color-black" />
+                           </InputGroupAddon>
+                       </InputGroup>
+                   </div>
+                    <div>
+                        <Button variant="outline">
+                            Search
+                        </Button>
+                    </div>
                 </div>
             </header>
         </>
