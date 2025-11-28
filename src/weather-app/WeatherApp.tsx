850bff7b69fb665fcfd8a4f06bfe326252d2aa20
anastasiam <anast.moud@gmail.com>
2025-11-28 21:35:44 +0200

    feat(WeatherApp): add inputRef and addQueryToInput handler to SearchField and render WeatherContainer only when loading is false

--- src/weather-app/WeatherApp.tsx
+++ src/weather-app/WeatherApp.tsx
@@ -2,15 +2,60 @@ import SearchField from "@/weather-app/components/SearchField.tsx";
 import WeatherContainer from "@/weather-app/components/WeatherContainer.tsx";
 import DateTable from "@/weather-app/components/DateTable.tsx";
 import {Theme} from "@radix-ui/themes";
+import {useEffect, useRef, useState} from "react";
+import {getCurrentWeather} from "@/weather-app/services/api.weather.ts";
+import type {WeatherDetailsProps} from "@/types/typesWeather.ts";
 
 const WeatherApp = () => {
+    const inputRef = useRef<HTMLInputElement>(null);
+    const [query, setQuery] = useState("");
+    const [loading, setLoading] = useState(true);
+    const [weather, setWeather] = useState<WeatherDetailsProps | null>();
+
+    const addQueryToInput = (value: string) => {
+        if (query.trim() !== "") {
+            setQuery(value);
+        }
+    }
+
+    //TODO: Add node.js for backend: CORS issue
+    useEffect(() => {
+        async function fetchWeather() {
+            try {
+                setLoading(true);
+                const data = await getCurrentWeather(query);
+                setWeather({
+                    temperature: data.main.temp,
+                    description: data.weather[0].description,
+                    country: data.sys.country,
+                    name: data.name,
+                });
+            } catch(err) {
+                console.error(err);
+            }
+        }
+        fetchWeather().catch(err => console.error("Unhandled promise:", err));
+        setLoading(false);
+        inputRef.current?.focus();
+    }, []);
+
     return (
         <>
        <Theme>
-           {/*  SearchField with search field  */}
-           <SearchField/>
-           {/*  Main Content  */}
-           <WeatherContainer/>
+           {/*  SearchField */}
+           <SearchField
+               inputRef={inputRef}
+               searchInput={addQueryToInput}
+
+           />
+           {!loading && (
+               <WeatherContainer
+                   temperature={weather?.temperature}
+                   description={weather?.description}
+                   country={weather?.country}
+                   name={weather?.name}
+               />
+           )}
            {/*  Footer: Table containing dates */}
            <DateTable/>
        </Theme>
