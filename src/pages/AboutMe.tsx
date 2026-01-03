9cfd0bc93aa746e58f2e513ca1d6f61f8a51c112
anastasiam <anast.moud@gmail.com>
2026-01-03 17:53:41 +0200

    feat(about): add AboutMeMainContent to the About Me page

--- src/pages/AboutMe.tsx
+++ src/pages/AboutMe.tsx
@@ -1,11 +1,13 @@
 import Header from "@/pages/layout/Header.tsx";
 import Footer from "@/pages/layout/Footer.tsx";
+import AboutMainContent from "@/pages/layout/AboutMainContent.tsx";
 
 const HomePage2 = () => {
     return (
         <>
-           <div className="relative">
+           <div className="relative h-screen">
                <Header />
+               <AboutMainContent />
                <Footer />
            </div>
         </>
