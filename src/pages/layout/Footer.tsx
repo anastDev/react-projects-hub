43b0d30c23e59a8f8afdb100f713c8322211340a
anastasiam <anast.moud@gmail.com>
2026-01-05 12:52:30 +0200

    style(footer): add background-white color and remove fixed position

--- src/pages/layout/Footer.tsx
+++ src/pages/layout/Footer.tsx
@@ -4,14 +4,19 @@ import { FaGithub } from "react-icons/fa";
 const Footer = () => {
     return (
         <>
-            <footer className="border-t border-black h-18 position fixed bottom-0 w-full">
+            <footer className="bg-white border-t border-black h-18 w-full">
                 <div className="flex flex-col items-center mt-2 space-x-2">
                    <div className="flex flex-row space-x-2">
                        <div>Home</div>
                        <div>About Me</div>
                    </div>
                     <div className="flex flex-row gap-2 mt-2">
-                       <div><FaLinkedin size="28px" /></div>
+                       <div>
+                           {/* Add href to the <a> tag*/}
+                           <a href="">
+                               <FaLinkedin size="28px" />
+                           </a>
+                       </div>
                         <div><FaGithub size="28px"/></div>
                     </div>
                 </div>
