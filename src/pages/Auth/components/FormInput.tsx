d9dbb73081e8d1ba78861419549147dc39650b0a
anastasiam <anast.moud@gmail.com>
2026-01-21 20:42:58 +0200

    feat(forms): extend FormInput to support select inputs and labels

--- src/pages/Auth/components/FormInput.tsx
+++ src/pages/Auth/components/FormInput.tsx
@@ -1,18 +1,51 @@
 import type {FormInputProps} from "@/pages/Auth/types/typesForm.ts";
 
-export const FormInput= ({type, name, value, placeholder, onChange, error} : FormInputProps) => {
+export const FormInput = ({
+                              type,
+                              name,
+                              value,
+                              placeholder,
+                              onChange,
+                              label,
+                              options = [],
+                          }: FormInputProps) => {
+
+    if (type === 'select') {
+        return (
+            <div className="space-y-1">
+                <label htmlFor={label} className="block text-sm font-medium">
+                    {label}
+                </label>
+                <select
+                    name={name}
+                    value={value}
+                    onChange={onChange}
+                    className="w-full border rounded-md px-4 py-2"
+                >
+                    <option value="">{placeholder || 'Select an option'}</option>
+                    {options.map((option) => (
+                        <option key={option.value} value={option.value}>
+                            {option.label}
+                        </option>
+                    ))}
+                </select>
+            </div>
+        );
+    }
     return (
-      <>
-          <input
-              type={type}
-              name={name}
-              value={value}
-              onChange={onChange}
-              placeholder={placeholder}
-              className="w-full border rounded-md px-4 py-2"
-              required/>
-          {error && <span className="text-red-500">{error}</span>}
-      </>
+        <div className="space-y-1">
+            <label htmlFor={label} className="block text-sm font-medium">
+                {label}
+            </label>
+            <input
+                type={type}
+                name={name}
+                value={value}
+                onChange={onChange}
+                placeholder={placeholder}
+                className="w-full border rounded-md px-4 py-2"
+            />
+        </div>
     );
 };
 
