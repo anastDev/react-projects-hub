7697dd0079fd2f1eb09ef3723e4951502394a9bf
anastasiam <anast.moud@gmail.com>
2026-01-21 21:48:59 +0200

    style(footer): center content and refine spacing and hover styles

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -7,24 +7,29 @@ const Footer = () => {
     return (
         <>
             <footer className="bg-white border-t shadow-sm w-full h-20 fixed bottom-0">
-                <div className="flex flex-col items-center mt-2 space-x-2">
-                   <div className="flex flex-row space-x-2">
+                <div className="flex flex-col mt-3">
+                   <div className="flex flex-row justify-center space-x-10">
                        {routes.map((route) => {
                            return (
-                               <div>
+                               <div className="hover:underline px-2">
                                    <Link to={route.path}>{route.title}</Link>
                                </div>
                            )
                        })}
                    </div>
-                    <div className="flex flex-row gap-2 mt-2">
+                    <div className="flex flex-row justify-center gap-4 mt-2">
                        <div>
                            {/* Add href to the <a> tag*/}
                            <a href="">
                                <FaLinkedin size="28px" />
                            </a>
                        </div>
-                        <div><FaGithub size="28px"/></div>
+                        <div>
+                            <a href="">
+                                <FaGithub size="28px"/>
+                            </a>
+                        </div>
+                        <div className="w-[10px]"></div>
                     </div>
                 </div>
             </footer>
