1c7e089923ca0a9ac8064882b158f45645a17512
anastasiam <anast.moud@gmail.com>
2026-01-30 17:30:28 +0100

    fix: fix issue preventing current weather data from being fetched

--- src/projects/weather-app/services/api.weather.ts
+++ src/projects/weather-app/services/api.weather.ts
@@ -1,20 +1,18 @@
-const API_URL = import.meta.env.WEATHER_API_URL_GEO;
-const API_KEY = import.meta.env.WEATHER_API_KEY;
+import type {WeatherApiResponse} from "@/projects/weather-app/types/typesWeather.tsx";
 
-export async function getCurrentWeather(cityName: string) {
+const WEATHER_API = import.meta.env.VITE_WEATHER_API;
+const API_KEY = import.meta.env.VITE_WEATHER_API_KEY;
+
+export async function getCurrentWeather(cityName: string) : Promise<WeatherApiResponse> {
     try {
-        const res = await fetch(`${API_URL}weather?q=${cityName}&units=metric&appid=${API_KEY}`);
+        const res = await fetch(`${WEATHER_API}weather?q=${cityName}&units=metric&appid=${API_KEY}`);
         console.log("Response status", res.status);
 
-        if (res.ok) {
-            const text = await res.text();
-            console.error("Fetch failed", text);
+        if (!res.ok) {
             throw new Error(`Failed to fetch weather ${res.status}`);
         }
 
-        const data = await res.json();
-        console.log("Received data", data);
-        return data;
+       return await res.json();
     } catch (err) {
         console.log("Error fetching weather: ",err);
         throw err;
