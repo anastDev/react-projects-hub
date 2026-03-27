80230ba549e238dad2264f943e6d48c530e19a74
anastasiam <anast.moud@gmail.com>
2026-03-27 22:42:11 +0100

    feat(Search): add back button to navigate to project page

--- src/projects/movie-search-app/components/Search.tsx
+++ src/projects/movie-search-app/components/Search.tsx
@@ -1,6 +1,7 @@
-import {SearchIcon} from "lucide-react";
+import {ArrowLeftIcon, SearchIcon} from "lucide-react";
 import {InputGroup, InputGroupAddon, InputGroupInput} from "@/components/ui/input-group.tsx";
 import {Button} from "@/components/ui/button.tsx"
+import {useNavigate} from "react-router";
 
 interface SearchMovieProps {
     movieInput: string;
@@ -10,11 +11,15 @@ interface SearchMovieProps {
 }
 
 const Search = ({movieInput, onchange, onSearch, reset}: SearchMovieProps) => {
+    const navigate = useNavigate();
 
     return (
         <>
-            <header>
+            <header className="flex flex-row space-x-2">
                 <div className="md:container mx-auto py-4 flex flex-row space-x-2">
+                    <Button variant="outline" size="icon" aria-label="Go Back" onClick={() => navigate("/projects")}>
+                        <ArrowLeftIcon size={28}/>
+                    </Button>
                    <div className="w-full flex-1">
                        <InputGroup className="bg-indigo-200">
                            <InputGroupInput placeholder="Search movie..." type="text" value={movieInput} onChange={onchange}/>
@@ -30,7 +35,7 @@ const Search = ({movieInput, onchange, onSearch, reset}: SearchMovieProps) => {
                             </Button>
                         </div>
                         <div>
-                            <Button variant="secondary" onClick={reset}>
+                            <Button variant="ghost" onClick={reset}>
                                Clear
                             </Button>
                         </div>
