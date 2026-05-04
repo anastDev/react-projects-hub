69befd442f613b1017cdc618ebb9af9a6f2979ff
anastasiam <anast.moud@gmail.com>
2026-05-04 19:21:55 +0200

    feat(types): extend road conditions with geometry and add deviation/accident types

--- src/projects/commute-risk-dashboard/types/typesRoadConditions.ts
+++ src/projects/commute-risk-dashboard/types/typesRoadConditions.ts
@@ -3,4 +3,25 @@ export interface RoadConditions {
     LocationText: string;
     ConditionText: string;
     ConditionInfo: string;
+    Geometry?: { WGS84: string };
+}
+
+export interface DeviationConditions {
+    AffectedDirection?: string;
+    CountyNo?: number[];
+    Geometry?: { WGS84: string }
+    Header?: string
+    IconId?: string
+    Message?: string
+    MessageType?: string
+    MessageTypeValue?: string
+    LocationDescriptor?: string
+    NumberOfLanesRestricted?: number
+    RoadNumber?: string
+    RoadName?: string
+    StartTime?: string
+    SeverityText?: string
+    Suspended?: boolean
+    TrafficRestrictionType?: string
+    PositionalDescription?: string
 }
