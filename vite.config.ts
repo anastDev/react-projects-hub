356fcb101a16623aef6c5d41781ff68796cd9d5e
anastasiam <anast.moud@gmail.com>
2026-07-07 13:02:20 +0200

    perf: divide the packages into smaller chunks for better performance

--- vite.config.ts
+++ vite.config.ts
@@ -15,8 +15,23 @@ export default defineConfig({
         rollupOptions: {
             output: {
                 manualChunks: {
-                    leaflet: ["leaflet"],
-                    motion: ["framer-motion"],
+                    'react-vendor': ['react', 'react-dom', 'react-router'],
+                    'leaflet-vendor': ['leaflet', 'react-leaflet', 'wellknown'],
+                    'motion-vendor': ['motion'],
+                    'radix-vendor': [
+                        '@radix-ui/themes',
+                        '@radix-ui/react-alert-dialog',
+                        '@radix-ui/react-dialog',
+                        '@radix-ui/react-hover-card',
+                        '@radix-ui/react-label',
+                        '@radix-ui/react-separator',
+                        '@radix-ui/react-slot',
+                        '@radix-ui/react-tooltip',
+                        'radix-ui',
+                    ],
+                    'three-vendor': ['three', 'three-stdlib', '@react-three/fiber', '@shadergradient/react'],
+                    'form-vendor': ['react-hook-form', '@hookform/resolvers', 'zod'],
+                    'misc-vendor': ['lucide-react', 'react-icons', 'sonner', 'js-cookie', 'jwt-decode', 'axios'],
                 },
             },
         },
